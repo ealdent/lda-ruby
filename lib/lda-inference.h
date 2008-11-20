ce4d29de4b1ecd5c8eb9c1b969a947e7772b0946
Jason Adams <jasonmadams@gmail.com>
2008-11-20 16:04:34 -0500

    attempting to fix build, won't compile with combo of lda-alpha and lda-inference

--- lib/lda-inference.h
+++ lib/lda-inference.h
@@ -1,11 +1,14 @@
 #ifndef LDA_INFERENCE_H
 #define LDA_INFERENCE_H
 
+#include <stdlib.h>
 #include <math.h>
 #include <float.h>
 #include <assert.h>
 #include "lda.h"
 #include "utils.h"
+
+
 
 #define NEWTON_THRESH 1e-5
 #define MAX_ALPHA_ITER 1000
@@ -28,6 +31,14 @@ double **last_gamma;
 
 enum BOOL { FALSE, TRUE } corpus_loaded, model_loaded, VERBOSE;
 #endif
+
+
+double alhood(double a, double ss, int D, int K);
+double d_alhood(double a, double ss, int D, int K);
+double d2_alhood(double a, int D, int K);
+double opt_alpha(double ss, int D, int K);
+void maximize_alpha(double** gamma, lda_model* model, int num_docs);
+
 
 double lda_inference(document*, lda_model*, double*, double**);
 double compute_likelihood(document*, lda_model*, double**, double*);
@@ -58,13 +69,5 @@ void infer(char* model_root,
            char* save,
            corpus* corpus);
            
-           
-
-double alhood(double a, double ss, int D, int K);
-double d_alhood(double a, double ss, int D, int K);
-double d2_alhood(double a, int D, int K);
-double opt_alpha(double ss, int D, int K);
-void maximize_alpha(double** gamma, lda_model* model, int num_docs);
-
 
 #endif
