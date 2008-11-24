24d546c2888d195637c9fc9244ff5363bd350020
Jason Adams <jasonmadams@gmail.com>
2008-11-24 15:13:07 -0500

    bug fix to wrap_get_phi

--- lib/lda-inference.c
+++ lib/lda-inference.c
@@ -853,11 +853,11 @@ static VALUE wrap_get_phi(VALUE self) {
     int max_length = max_corpus_length(last_corpus);
     
     for (i = 0; i < last_corpus->num_docs; i++) {
-        VALUE arr1 = rb_ary_new2(last_corpus->docs[i]->length);
+        VALUE arr1 = rb_ary_new2(last_corpus->docs[i].length);
         
-        lda_inference(&(last_corpus->docs[i]), last_model, last_gamma[d], last_phi);
+        lda_inference(&(last_corpus->docs[i]), last_model, last_gamma[i], last_phi);
         
-        for (j = 0; j < last_corpus->docs[i]->length; j++) {
+        for (j = 0; j < last_corpus->docs[i].length; j++) {
             VALUE arr2 = rb_ary_new2(last_model->num_topics);
             
             for (k = 0; k < last_model->num_topics; k++) {
